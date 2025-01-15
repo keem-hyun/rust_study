43bf100cfeb5a2213382362642068c31fcc610f3
keem-hyun <hw813@naver.com>
2025-01-15 23:59:06 +0900

    rust docs: 4.3 ownership

--- projects/ownership/src/main.rs
+++ projects/ownership/src/main.rs
@@ -1,11 +1,38 @@
 fn main() {
-    let s1 = String::from("hello");
+    let my_string = String::from("hello world");
 
-    let len = calculate_length(&s1);
+    let word = first_word(&my_string[0..6]);
+    let word = first_word(&my_string[..]);
+    let word = first_word(&my_string);
 
-    println!("The length of '{}' is {}.", s1, len);
+    let my_string_literal = "hello world";
+
+    let word = first_word(&my_string_literal[0..6]);
+    let word = first_word(&my_string_literal[..]);
+    let word = first_word(my_string_literal);
+    let word = first_word(my_string_literal);
 }
 
-fn calculate_length(s: &String) -> usize {
-    s.len()
+fn first_word(s: &str) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
+}
+
+fn second_word(s: &String) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[i + 1..];
+        }
+    }
+
+    &s[..]
 }
